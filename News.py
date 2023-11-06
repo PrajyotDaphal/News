fd63a24b144607b988c8d8c7f9c5aea9d8ba4863
Beta_Dext <87372653+Unkown-Bug@users.noreply.github.com>
2023-11-06 20:41:04 +0530

    Add files via upload

---
+++ News.py
@@ -0,0 +1,105 @@
+import requests
+from features.Listen import Listen
+from features.Speak import Say
+
+apikey = "b7c98fb3391e484eb7719186558589ff"
+
+def business():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=business&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])   
+
+      for i in range(10):
+        Say(newsarticle[i]) 
+
+def science():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=science&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])
+
+      for i in range(10):
+        Say(newsarticle[i])        
+
+def entertainment():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])
+
+      for i in range(10):
+        Say(newsarticle[i]) 
+
+def sports():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])
+
+      for i in range(10):
+        Say(newsarticle[i])  
+
+def health():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=health&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])
+
+      for i in range(10):
+        Say(newsarticle[i])
+
+def technology():
+      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apikey=" + str(apikey)
+      news = requests.get(mainurl).json()
+      article = news['articles']
+      newsarticle = []
+      for arti in article:
+        newsarticle.append(arti['title'])
+
+      for i in range(10):
+        Say(newsarticle[i])
+
+while True:
+   try:
+
+    branch = Listen()
+
+    if "business" in branch:
+     business()
+     Say("wan another news")
+     
+
+    elif "science" in branch:
+     science()
+     Say("wan another news")
+
+    elif "technology" in branch:
+      technology()
+      Say("wan another news")
+
+    elif "entertainment" in branch:
+     entertainment() 
+     Say("wan another news")
+
+    elif "sports" in branch:
+     sports()
+     Say("wan another news")
+
+    else:
+       Say("Unknown Category")
+       break
+
+   except:
+    Say("Unknown Category")
+    continue                               
