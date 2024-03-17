f5b912ca98b2418617cb56b355ecbddeea5dc2c5
Beta_Dext <87372653+Unkown-Bug@users.noreply.github.com>
2024-03-18 03:01:40 +0530

    News.py
    By using URLs from JSON File
    
    Signed-off-by: Beta_Dext <87372653+Unkown-Bug@users.noreply.github.com>

--- News.py
+++ News.py
@@ -1,105 +1,46 @@
 import requests
-from Listen import Listen
-from Speak import Say
+import json
+from Extention.Speak import speak
 
-apikey = "***PAST YOUR API KEY***"
+def get_urls():
+    with open('## Contents.json File Location ##', 'r') as file:
+        return json.load(file)
 
-def business():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=business&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])   
-
-      for i in range(10):
-        Say(newsarticle[i]) 
-
-def science():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=science&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])
-
-      for i in range(10):
-        Say(newsarticle[i])        
-
-def entertainment():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])
-
-      for i in range(10):
-        Say(newsarticle[i]) 
-
-def sports():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=sports&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])
-
-      for i in range(10):
-        Say(newsarticle[i])  
-
-def health():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=health&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])
-
-      for i in range(10):
-        Say(newsarticle[i])
-
-def technology():
-      mainurl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apikey=" + str(apikey)
-      news = requests.get(mainurl).json()
-      article = news['articles']
-      newsarticle = []
-      for arti in article:
-        newsarticle.append(arti['title'])
-
-      for i in range(10):
-        Say(newsarticle[i])
-
-while True:
-   try:
-
-    branch = Listen()
-
-    if "business" in branch:
-     business()
-     Say("wan another news")
-     
-
-    elif "science" in branch:
-     science()
-     Say("wan another news")
-
-    elif "technology" in branch:
-      technology()
-      Say("wan another news")
-
-    elif "entertainment" in branch:
-     entertainment() 
-     Say("wan another news")
-
-    elif "sports" in branch:
-     sports()
-     Say("wan another news")
-
+def News(category):
+    urls = get_urls()
+    if category in urls.get('news_URL', {}):
+        url = urls['news_URL'][category]
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            news_data = response.json()
+            articles = news_data.get('articles', [])
+            
+            for i in range(min(10, len(articles))):
+                article = articles[i]
+                speak("Title:", article.get('title', 'N/A'))
+                speak("Description:", article.get('description', 'N/A'))
+                print()
+        else:
+            speak(f"Failed to fetch news. Status Code: {response.status_code}")
     else:
-       Say("Unknown Category")
-       break
+        speak("Invalid news category")
 
-   except:
-    Say("Unknown Category")
-    continue                               
+def extract_category(command):
+    category_keywords = ['jarvis news on', 'news in']
+    for keyword in category_keywords:
+        if keyword in command:
+            return command.split(keyword, 1)[-1].strip()
+    return None
+
+
+    while True:
+    # if 1:
+     query = Listen().lower()    
+
+     if "news" in query:
+          category = extract_category(query)
+          if category:
+           News(category)
+          else:
+              speak("Sorry, I couldn't determine the news category.")
